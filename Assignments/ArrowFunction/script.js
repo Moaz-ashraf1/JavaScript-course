49545646e8ce7fd7de456924f65e8a256507e3e3
moaz ashraf <101815877+Moaz-ashraf1@users.noreply.github.com>
2022-08-04 20:21:27 +0200

    first commit 🚀

---
+++ Assignments/ArrowFunction/script.js
@@ -0,0 +1,2 @@
+const percentageOfWorld3 = (population) => (population / 7900) * 100;
+console.log(percentageOfWorld3(10));
---
+++ Assignments/functionDeclarationsAndExpressions/script.js
@@ -0,0 +1,9 @@
+console.log(percentageOfWorld1(1441));
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+console.log(percentageOfWorld1(1441));
