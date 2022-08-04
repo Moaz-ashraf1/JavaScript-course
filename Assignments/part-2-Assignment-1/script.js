601a10c2bf37f31d489fd159a2ae99671f4f7b07
moaz ashraf <101815877+Moaz-ashraf1@users.noreply.github.com>
2022-08-04 04:57:48 +0200

    first commit 🚀

---
+++ Assignments/part-2-Assignment-1/script.js
@@ -0,0 +1,5 @@
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} people and its 
+capital city is ${capitalCity}`;
+}
+console.log(describeCountry("Egypt", "6 million", "Egypt"));
